087ecd87e38e097a2f8e29eb495420bea5380ab6
unknown <mayordarien@gmail.com>
2024-05-01 11:40:32 -0600

    Tarea, sesion 17 - Darien Mayorga

--- src/App.java
+++ src/App.java
@@ -1,16 +1,22 @@
+//Darien José Mayorga Moreno
+//2024-1356U
+
 import java.util.Scanner;
+
 
 public class App {
 
+
+    //variables globales
     public static int tamaño = 0;
     public static Scanner leer = new Scanner(System.in);
-    public static int aux = 0;
-    
+    public static boolean condicion = true;
+
 
     public static void main(String[] args) throws Exception {
 
         boolean condicion = true;
-        
+        //Trycatch para asegurarnos que se ingresen los datos correctos
         do {
             try {
                 System.out.println("Ingrese el tamaño del arreglo, no puede ser mayor que 16 y mayor que 1");
@@ -41,12 +47,23 @@ public class App {
 
     }
     //metodo para llenar el arreglo
+    
     public static int [] llenado(){
         int [] arreglo = new int [tamaño];
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.println("ingrese el valor [" + (i+1) + "]");
-            arreglo[i] = leer.nextInt();
-        }
+        do {
+            try {
+                for (int i = 0; i < arreglo.length; i++) {
+                    System.out.println("ingrese el valor [" + (i+1) + "]");
+                    arreglo[i] = leer.nextInt();
+                  
+                }
+                condicion=false;
+            } catch (Exception e) {
+                System.out.println("Ingrese valores correctos");
+                leer.nextLine();
+            }
+        } while (condicion);
+        
         return arreglo;
     }
     //metodo para mostrarlo
@@ -56,6 +73,7 @@ public class App {
         }
         System.out.println(" ");
     }
+    //metodo para mostrar el arreglo inverso
     public static void inverso (int[]arregloinverso){
 
             for (int i = arregloinverso.length - 1; i >= 0; i--) {
